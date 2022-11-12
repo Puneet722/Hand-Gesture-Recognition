069ad6bf973c41aa290e90713052d56acd7c360f
Puneet Raghwani <puneetofficial722@gmail.com>
2022-11-13 01:53:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import tensorflow as tf
 from tensorflow.keras.models import load_model
 
 mpHands = mp.solutions.hands
-hands = mpHands.Hands(
-max_num_hands=1, min_detection_confidence=0.8, min_tracking_confidence=0.5)
 mpDraw = mp.solutions.drawing_utils
+hands = mpHands.Hands(
+max_num_hands=1,
+min_detection_confidence=0.8, 
+min_tracking_confidence=0.5)
 
 model = load_model('mp_hand_gesture')
 
@@ -20,12 +22,8 @@ video = cv2.VideoCapture(1)
 
 while True:
     check, frame = video.read()
-
     x, y, c = frame.shape
-
-
     framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
     result = hands.process(framergb)
 
     className = ''
@@ -36,11 +34,8 @@ while True:
             for lm in handslms.landmark:
                 lmx = int(lm.x * x)
                 lmy = int(lm.y * y)
-
                 landmarks.append([lmx, lmy])
-
             mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
-
             prediction = model.predict([landmarks])
 
             classID = np.argmax(prediction)
@@ -48,11 +43,9 @@ while True:
 
     cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX,
                 1, (0, 0, 255), 2, cv2.LINE_AA)
-
     cv2.imshow("Output", frame)
 
     key = cv2.waitKey(1)
-
     if key == ord('q'):
         break
 
